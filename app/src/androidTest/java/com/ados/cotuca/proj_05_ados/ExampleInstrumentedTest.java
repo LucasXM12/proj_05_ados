bd2318325985b9a1dcb7644a13471db3b9e86041
LucasXM12 <lucasxm21@hotmail.com>
2017-05-18 09:16:29 -0300

    Primeiro commit

---
+++ app/src/androidTest/java/com/ados/cotuca/proj_05_ados/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.ados.cotuca.proj_05_ados;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.ados.cotuca.proj_05_ados", appContext.getPackageName());
+    }
+}
